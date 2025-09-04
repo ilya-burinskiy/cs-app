0a2fa190308f0f5b16edd5e0a678b41db307683f
Ilya Burinsky <burinskiy992@gmail.com>
2025-09-04 08:20:49 +0300

    2.75

--- 2/2.75.c
+++ 2/2.75.c
@@ -1,40 +1,45 @@
 #include <assert.h>
 #include <limits.h>
+#include <inttypes.h>
+#include <stdio.h>
 
 unsigned unsigned_high_prod(unsigned, unsigned);
+unsigned unsigned_high_prod2(unsigned, unsigned);
 int signed_high_prod(int, int);
 
 int main(int argc, char **argv) {
-  unsigned x, y, lower_bits, high_bits;
-  y = x = UINT_MAX;
-  lower_bits = x * y;
-  high_bits = unsigned_high_prod(x, y);
-  assert(lower_bits == );
-  assert(high_bits == 1);
+  unsigned x1, y1, z1, z2;
+  x1 = 0x7FFFFFFFU;
+  y1 = 0x12345678U;
+  z1 = unsigned_high_prod(x1, y1);
+  z2 = unsigned_high_prod2(x1, y1);
+  assert(z1 == z2);
   return 0;
 }
 
 /*
- *          1111
- *          1111
- *          ----
- *          1111
- *         1111
- *        1111
- *       1111
- *       -------
- *    1101010101
- * (0) 1
- * (0) 1 + 1 = 10
- * (10) 1 + 1 + 1 = 11 + 10 = 101
- * (100) 1 + 1 + 1 + 1 = 100 + 100 = 1000
- * (1000) 1 + 1 + 1 = 11 + 1000 = 1011
- * (1010) 1 + 1 = 10 + 1010 = 1100
+ * T2U x | x < 0  = x + 2^w
+ *       | x >= 0 = x
+ *
+ * x * y                                   <- lower w bits
+ * u = x_w-1*y + y_w-1*x + x_w-1*y_w-1*2^w
+ *     -----------------   ---------------
+ *        signed high       cast to U part
+ *           prod
  * */
 unsigned unsigned_high_prod(unsigned x, unsigned y) {
-  return 1;
+  int high32bits = signed_high_prod((int) x, (int) y);
+  int xmsb = x >> 31;
+  int ymsb = y >> 31;
+  return high32bits + xmsb * y + ymsb * x;
+}
+
+unsigned unsigned_high_prod2(unsigned x, unsigned y) {
+  uint64_t res = (uint64_t) x * y;
+  return res >> 32;
 }
 
 int signed_high_prod(int x, int y) {
-  return 1;
+  int64_t res = (int64_t) x * y;
+  return res >> 32;
 }
