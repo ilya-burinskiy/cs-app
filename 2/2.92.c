5d95967118d96557981d44e131b0aa71a902f8cc
Ilya Burinskij <burinskiy992@gmail.com>
2025-11-11 12:37:20 +0300

    2.92 fixup

--- 2/2.92.c
+++ 2/2.92.c
@@ -1,28 +1,38 @@
 #include <assert.h>
 #include <limits.h>
+#include <stdio.h>
+#include <math.h>
 
 typedef unsigned float_bits;
 
 float_bits float_negate(float_bits f);
 float u2f(unsigned u);
+unsigned f2u(float);
 
 int main(int argc, char **argv) {
-  unsigned not_eq_cnt = 0;
-  float expected, f_neg_u;
-  for (unsigned u = 0; u < UINT_MAX; ++u) {
-    expected = -u2f(u);
-    f_neg_u = u2f(float_negate((float_bits) u));
-    if (expected != f_neg_u) {
-      ++not_eq_cnt;
+  unsigned errors = 0;
+  float expected, neg_f;
+  for (unsigned f_bin = 0; f_bin < UINT_MAX; ++f_bin) {
+    expected = -u2f(f_bin);
+    neg_f = u2f(float_negate(f_bin));
+    if (isnan(expected) && isnan(neg_f)) {
+      continue;
+    }
+
+    if (expected != neg_f) {
+      printf("Mismatch for 0x%X:\n", f_bin);
+      printf("Expected: 0x%X, got: 0x%X\n", f2u(expected), f2u(neg_f));
+      errors++;
+      if (errors > 10) {
+        break;
+      }
     }
   }
-  expected = -u2f(UINT_MAX);
-  f_neg_u = u2f(float_negate((float_bits) UINT_MAX));
-  if (expected != f_neg_u) {
-    ++not_eq_cnt;
-  }
-  // 2^24 - 2; +inf, -inf
-  assert(not_eq_cnt == (1 << 24) - 2);
+
+  expected = u2f(UINT_MAX) * 0.5;
+  neg_f = u2f(float_negate(UINT_MAX));
+  assert(isnan(expected) && isnan(neg_f));
+
   return 0;
 }
 
@@ -30,16 +40,13 @@ float_bits float_negate(float_bits f) {
   unsigned sign = f >> 31;
   unsigned exp = (f >> 23) & 0xFF;
   unsigned frac = f & 0x7FFFFF;
-  float_bits res;
-  if (exp == 0xFF && frac != 0) {
-    // NaN
-    res = f;
-  } else {
-    res = ((!sign) << 31) | (exp << 23) | frac;
-  }
-  return res;
+  return ((!sign) << 31) | (exp << 23) | frac;
 }
 
 float u2f(unsigned u) {
   return *((float *) &u);
+}
+
+unsigned f2u(float f) {
+  return *((unsigned *) &f);
 }
