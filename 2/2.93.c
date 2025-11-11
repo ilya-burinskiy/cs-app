2ad19bf31af678f3ccc8483bb4b1d14acb3b5038
Ilya Burinskij <burinskiy992@gmail.com>
2025-11-11 12:23:49 +0300

    2.93 fixup

--- 2/2.93.c
+++ 2/2.93.c
@@ -1,34 +1,41 @@
 #include <assert.h>
 #include <limits.h>
 #include <stdio.h>
+#include <math.h>
 
 typedef unsigned float_bits;
 
 float_bits float_absval(float_bits);
 float u2f(unsigned);
+unsigned f2u(float);
 
 int main(int argc, char **argv) {
-  unsigned not_eq_cnt = 0;
-  float expected, f_abs_u;
-  for (unsigned u = 0; u < UINT_MAX; ++u) {
-    expected = u2f(u);
+  unsigned errors = 0;
+  float expected, abs_f;
+  for (unsigned f_bin = 0; f_bin < UINT_MAX; ++f_bin) {
+    expected = u2f(f_bin);
     if (expected < 0) {
       expected = -expected;
     }
-    f_abs_u = u2f(float_absval((float_bits) u));
-    if (expected != f_abs_u) {
-      ++not_eq_cnt;
+    abs_f = u2f(float_absval(f_bin));
+    if (isnan(expected) && isnan(abs_f)) {
+      continue;
+    }
+
+    if (expected != abs_f) {
+      printf("Mismatch for 0x%X:\n", f_bin);
+      printf("Expected: 0x%X, got: 0x%X\n", f2u(expected), f2u(abs_f));
+      errors++;
+      if (errors > 10) {
+        break;
+      }
     }
   }
-  expected = u2f(UINT_MAX);
-  if (expected < 0) {
-    expected = -expected;
-  }
-  f_abs_u = u2f(float_absval((float_bits) UINT_MAX));
-  if (expected != f_abs_u) {
-    ++not_eq_cnt;
-  }
-  assert(not_eq_cnt == (1 << 24) - 2);
+
+  expected = u2f(UINT_MAX) * 0.5;
+  abs_f = u2f(float_absval(UINT_MAX));
+  assert(isnan(expected) && isnan(abs_f));
+
   return 0;
 }
 
@@ -36,17 +43,16 @@ float_bits float_absval(float_bits f) {
   unsigned sign = f >> 31;
   unsigned exp = (f >> 23) & 0xFF;
   unsigned frac = f & 0x7FFFFF;
-  float_bits res;
-  if (exp == 0xFF && frac != 0) {
-    res = f;
-  } else if (sign) {
-    res = (exp << 23) | frac;
-  } else {
-    res = f;
+  if (sign) {
+    sign = 0;
   }
-  return res;
+  return (sign << 31) | (exp << 23) | frac;
 }
 
 float u2f(unsigned u) {
   return *((float *) &u);
+}
+
+unsigned f2u(float f) {
+  return *((unsigned *) &f);
 }
